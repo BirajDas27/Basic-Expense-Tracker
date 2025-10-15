30af91b4aa07db30dd04d8934872dea83723f7cc
BirajDas27 <birajdas27092002@gmail.com>
2025-10-15 23:30:31 +0530

    CTk

---
+++ CTkExpenseTracker.py
@@ -0,0 +1,38 @@
+import customtkinter as ctk
+
+ctk.set_appearance_mode = "system"
+ctk.set_default_color_theme = "blue"
+
+app = ctk.CTk()
+
+app.title('Expense Tracker')
+app.geometry('900x600')
+
+navbar = ctk.CTkFrame(app,
+    width = 900,
+    height = 80,
+    fg_color = '#600080'
+)
+navbar.pack()
+
+my_label = ctk.CTkLabel(navbar,
+    text = "Expense Tracker UI",
+    font = ("Helvenica", 25, "bold"),
+    text_color = "white",
+    padx = 900,
+    pady = 15
+
+)
+my_label.pack()
+
+sidebar = ctk.CTkFrame(app,
+    fg_color = 'red',
+    width = 90,
+    corner_radius = 100,
+    height = 480,
+    
+    
+)
+sidebar.pack(pady = 15)
+
+app.mainloop()
