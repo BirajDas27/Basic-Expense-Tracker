03f651839aba1337d9a375c80a977efed7a2178e
BirajDas27 <birajdas27092002@gmail.com>
2025-10-19 00:54:40 +0530

    icons and sidebar updated

--- CTkExpenseTracker.py
+++ CTkExpenseTracker.py
@@ -1,4 +1,5 @@
 import customtkinter as ctk
+from PIL import Image, ImageTk
 
 ctk.set_appearance_mode = "system"
 ctk.set_default_color_theme = "blue"
@@ -7,12 +8,12 @@ app = ctk.CTk()
 
 app.title('Expense Tracker')
 app.geometry('800x500')
-app.configure(fg_color = "#260731")
+app.configure(fg_color = "#BCDCE6")
 
 navbar = ctk.CTkFrame(app,
     width = 900,
     height = 200,
-    fg_color = '#993B9C'
+    fg_color = '#2b6777',
 )
 navbar.pack()
 
@@ -27,18 +28,88 @@ my_label = ctk.CTkLabel(navbar,
 my_label.pack()
 
 sidebar = ctk.CTkFrame(app,
-    fg_color = '#683B9C',
-    width = 70,
-    corner_radius = 150,
+    fg_color = '#52ab98',
+    width = 60,
+    corner_radius = 20,
     height = 450,
+    border_width = 1,
+    border_color = "#87C3D4",
 )
 sidebar.pack(padx = (15, 0), pady = (15, 20), side = 'left')
 
+inner_sidebar = ctk.CTkFrame(sidebar,
+    fg_color = 'transparent',
+    corner_radius = 20,
+    width = 60
+)
+inner_sidebar.pack(padx = 2,pady = 30)
+
+image_path = "screenshots/icons/wallet.png"
+my_image = Image.open(image_path)
+add_icon = ctk.CTkImage(light_image=my_image, size=(20, 20))
+
+image_path = "screenshots/icons/"
+my_image = Image.open(image_path)
+view_icon = ctk.CTkImage(light_image=my_image, size=(20, 20))
+
+image_path = "screenshots/icons/"
+my_image = Image.open(image_path)
+update_icon = ctk.CTkImage(light_image=my_image, size=(20, 20))
+
+image_path = "screenshots/icons/"
+my_image = Image.open(image_path)
+remove_icon = ctk.CTkImage(light_image=my_image, size=(20, 20))
+
+image_path = "screenshots/icons/"
+my_image = Image.open(image_path)
+total_icon = ctk.CTkImage(light_image=my_image, size=(20, 20))
+
+image_path = "screenshots/icons/"
+my_image = Image.open(image_path)
+search_icon = ctk.CTkImage(light_image=my_image, size=(20, 20))
+
+image_path = "screenshots/icons/"
+my_image = Image.open(image_path)
+sort_icon = ctk.CTkImage(light_image=my_image, size=(20, 20))
+
+image_path = "screenshots/icons/"
+my_image = Image.open(image_path)
+periodic_icon = ctk.CTkImage(light_image=my_image, size=(20, 20))
+
+# Define button options (first one uses image)
+options = [
+    {"image": add_icon, "text": ""},
+    {"image": view_icon, "text": ""},
+    {"image": update_icon, "text": ""},
+    {"image": remove_icon, "text": ""},
+    {"image": total_icon, "text": ""},
+    {"image": search_icon, "text": ""},
+    {"image": sort_icon, "text": ""},
+    {"image": periodic_icon, "text": ""},
+]
+
+# Create buttons
+for opt in options:
+    
+    my_button = ctk.CTkButton(
+        inner_sidebar,
+        image=opt["image"],
+        text="",  # no text for image
+        fg_color="transparent",
+        bg_color="transparent",
+        width=50,
+        height=40,
+        hover_color="#2b6777"
+    )
+    my_button.pack()
+    
+
+
 main_container = ctk.CTkFrame(app,
-    border_width = 2,
-    border_color = "#CF48A9",
+    border_width = 1,
+    border_color = "#2b6777",
     corner_radius = 15,
-    fg_color = "#937296",
+    fg_color = "#f2f2f2",
     height = 450
 )
 main_container.pack(padx = (15, 15), pady = (15, 20), fill = 'both')
