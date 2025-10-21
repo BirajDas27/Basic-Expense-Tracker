5a72d0ee6d78f13e5a6e33e696866c7572ded189
BirajDas27 <birajdas27092002@gmail.com>
2025-10-22 00:48:08 +0530

    Update CTkExpenseTracker.py

--- CTkExpenseTracker.py
+++ CTkExpenseTracker.py
@@ -7,7 +7,7 @@ ctk.set_default_color_theme = "blue"
 app = ctk.CTk()
 
 app.title('Expense Tracker')
-app.geometry('800x500')
+app.geometry('890x590')
 app.configure(fg_color = "#BCDCE6")
 app.resizable(True, True)
 
@@ -31,8 +31,8 @@ my_label.pack()
 sidebar = ctk.CTkFrame(app,
     fg_color = '#52ab98',
     width = 60,
-    corner_radius = 30,
-    height = 450,
+    corner_radius = 15,
+    height = 630,
     border_width = 1,
     border_color = "#87C3D4",
 )
@@ -43,7 +43,7 @@ inner_sidebar = ctk.CTkFrame(sidebar,
     corner_radius = 20,
     width = 60
 )
-inner_sidebar.pack(padx = 2,pady = 30)
+inner_sidebar.pack(padx = 2,pady = 40)
 
 image_path = "screenshots/icons/wallet.png"
 my_image = Image.open(image_path)
@@ -98,7 +98,7 @@ add_button = ctk.CTkButton(
     hover_color="#2b6777",
     command = add
 )
-add_button.pack(pady = 5)
+add_button.pack(pady = 7)
 add_button.bind("<Enter>", add_on_enter)
 add_button.bind("<Leave>", add_on_leave)
 
@@ -122,7 +122,7 @@ view_button = ctk.CTkButton(
     hover_color="#2b6777",
     command = view
 )
-view_button.pack(pady = 5)
+view_button.pack(pady = 7)
 view_button.bind("<Enter>", view_on_enter)
 view_button.bind("<Leave>", view_on_leave)
 
@@ -146,7 +146,7 @@ update_button = ctk.CTkButton(
     hover_color="#2b6777",
     command = update
 )
-update_button.pack(pady = 5)
+update_button.pack(pady = 7)
 update_button.bind("<Enter>", update_on_enter)
 update_button.bind("<Leave>", update_on_leave)
 
@@ -171,7 +171,7 @@ remove_button = ctk.CTkButton(
     hover_color="#2b6777",
     command = remove
 )
-remove_button.pack(pady = 5)
+remove_button.pack(pady = 7)
 remove_button.bind("<Enter>", remove_on_enter)
 remove_button.bind("<Leave>", remove_on_leave)
 
@@ -196,7 +196,7 @@ total_button = ctk.CTkButton(
     hover_color="#2b6777",
     command = total
 )
-total_button.pack(pady = 5)
+total_button.pack(pady = 7)
 total_button.bind("<Enter>", total_on_enter)
 total_button.bind("<Leave>", total_on_leave)
 
@@ -221,7 +221,7 @@ search_button = ctk.CTkButton(
     hover_color="#2b6777",
     command = search
 )
-search_button.pack(pady = 5)
+search_button.pack(pady = 7)
 search_button.bind("<Enter>", search_on_enter)
 search_button.bind("<Leave>", search_on_leave)
 
@@ -246,7 +246,7 @@ sort_button = ctk.CTkButton(
     hover_color="#2b6777",
     command = sort
 )
-sort_button.pack(pady = 5)
+sort_button.pack(pady = 7)
 sort_button.bind("<Enter>", sort_on_enter)
 sort_button.bind("<Leave>", sort_on_leave)
 
@@ -271,7 +271,7 @@ periodic_button = ctk.CTkButton(
     hover_color="#2b6777",
     command = periodic
 )
-periodic_button.pack(pady = 5)
+periodic_button.pack(pady = 7)
 periodic_button.bind("<Enter>", periodic_on_enter)
 periodic_button.bind("<Leave>", periodic_on_leave)
     
@@ -324,7 +324,7 @@ container2 = ctk.CTkFrame(
     fg_color = '#C0DAF0',
     corner_radius = 15,
     border_width = 0,
-    height = 600
+    height = 430
 
 )
 container2.pack(padx = 10, pady = 15, fill = 'both')
@@ -355,6 +355,75 @@ features = ctk.CTkLabel(
 )
 features.pack(padx = (10, 5), pady = (0, 0))
 
+image_path = "screenshots/icons/wallet.png"
+my_image = Image.open(image_path)
+add_icon = ctk.CTkImage(light_image=my_image, size=(60, 60))
+
+image_path = "screenshots/icons/search.png"
+my_image = Image.open(image_path)
+view_icon = ctk.CTkImage(light_image=my_image, size=(60, 60))
+
+image_path = "screenshots/icons/changes.png"
+my_image = Image.open(image_path)
+update_icon = ctk.CTkImage(light_image=my_image, size=(60, 60))
+
+image_path = "screenshots/icons/trash.png"
+my_image = Image.open(image_path)
+remove_icon = ctk.CTkImage(light_image=my_image, size=(60, 60))
+
+image_path = "screenshots/icons/sum.png"
+my_image = Image.open(image_path)
+total_icon = ctk.CTkImage(light_image=my_image, size=(60, 60))
+
+image_path = "screenshots/icons/magnifier.png"
+my_image = Image.open(image_path)
+search_icon = ctk.CTkImage(light_image=my_image, size=(60, 60))
+
+image_path = "screenshots/icons/sort.png"
+my_image = Image.open(image_path)
+sort_icon = ctk.CTkImage(light_image=my_image, size=(60, 60))
+
+image_path = "screenshots/icons/calendar.png"
+my_image = Image.open(image_path)
+periodic_icon = ctk.CTkImage(light_image=my_image, size=(60, 60))
+
+feature1 = ctk.CTkFrame(
+    container2,
+    fg_color = "#CCDDED",
+    height = 550,
+)
+feature1.pack(padx = 10, pady = 10, fill = 'both')
+feature1.pack_propagate(False)
+
+for i in range(4):
+    feature1.grid_columnconfigure(i, weight = 1)
+
+iconlist = [add_icon, view_icon, update_icon, remove_icon, total_icon, search_icon, sort_icon, periodic_icon]
+labels = ["Add", "View", "Update", "Remove", "Total", "Search", "Sort", "Periodic"]
+
+for i, (icon, text) in enumerate(zip(iconlist, labels)):
+    row = 0 if i < 4 else 2
+    col = i % 4
+
+    icon_label = ctk.CTkLabel(
+        feature1,
+        image=icon,
+        text="",
+        text_color="black",
+        fg_color="transparent",
+        font=('Helvetica', 100, 'bold'),
+        height=100,
+        width=100
+    )
+    icon_label.grid(row=row, column=col, pady=(5, 0))
+
+    text_label = ctk.CTkLabel(
+        feature1,
+        text=text,
+        text_color="black",
+        font=("Helvetica", 15, 'bold')
+    )
+    text_label.grid(row=row + 1, column=col, pady=(0, 10))
 
 
 app.mainloop()
